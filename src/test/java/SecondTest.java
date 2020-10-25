cc5398b9cef69d4fb898c07dbea39c24d23a4ff7
mahmoudmohamed94 <mahmoud.rashwan611@gmail.com>
2020-10-25 02:29:00 +0200

    Add a fifth , a sixth and a seventh test

--- src/test/java/SecondTest.java
+++ src/test/java/SecondTest.java
@@ -1,7 +1,22 @@
+import org.testng.annotations.Test;
+
 public class SecondTest{
 
     @Test
     public void lisaTest4(){
         
+    }
+
+    @Test
+    public void lisaTest5(){
+     
+    }
+    @Test
+    public void lisaTest6(){
+
+    }
+    @Test
+    public void lisaTest7(){
+
     }
 }
